0709cf6d3d956b9f9305abd8a2be8abb432aaa4f
Lasun Alexandr <lasunaleksandr@gmail.com>
2024-08-21 00:42:57 +0300

    eshe razochek

--- src/api/socket-api.ts
+++ src/api/socket-api.ts
@@ -4,8 +4,8 @@ class SocketApi {
   static socket: null | Socket = null
 
   static createConnection() {
-    // this.socket = io('https://chatt-server.onrender.com')
-    this.socket = io('http://192.168.0.106:3001')
+    this.socket = io('https://chatt-server.onrender.com')
+    // this.socket = io('http://192.168.0.106:3001')
 
     this.socket.on('connect', () => {
       console.log('connected')
