ad4bb2314238b499cbe75d22a3eecb3f1016a128
Lasun Alexandr <lasunaleksandr@gmail.com>
2024-12-02 23:16:50 +0200

    upd url

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { getTokenFromLocalStorage } from '../helpers/localstorage.helper'
 export const instance = axios.create({
-  // baseURL: 'https://chatt-server.onrender.com/api',
-  baseURL: 'http://192.168.0.104:3001/api',
+  baseURL: 'https://chatt-server.onrender.com/api',
+  // baseURL: 'http://192.168.0.104:3001/api',
   headers: {
     Authorization: 'Bearer ' + getTokenFromLocalStorage(),
   },
